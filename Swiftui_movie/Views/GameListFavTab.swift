08b5cb860f3fb8d4bddf41a95c8de52d749cf0b1
Aji Suryawan <kazumovie132@gmail.com>
2020-07-30 23:39:34 +0700

    fixing

--- Swiftui_movie/Views/GameListFavTab.swift
+++ Swiftui_movie/Views/GameListFavTab.swift
@@ -9,9 +9,34 @@ struct GameListFavTab: View {
     var body: some View {
         //NavigationView {
         VStack{
-            Text("makanan")
+            Image("aji")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+                .padding()
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    
+                    Text("name")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
+                    
+                    Text("email")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)))
+                    
+                    
+                }.layoutPriority(100)
+                
+                Spacer()
+            }.padding()
             
-        }        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        }   .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
             .background(Color(.white))
             .navigationBarTitle("My Profile",displayMode: .inline)
         
@@ -24,3 +49,4 @@ struct GameListFavTab_Previews: PreviewProvider {
         GameListFavTab()
     }
 }
+
