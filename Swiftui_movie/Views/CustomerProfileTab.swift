1e7521dc268c57fa1b6f945bc3a6ee0de1e66bce
Aji Suryawan <kazumovie132@gmail.com>
2020-08-04 17:37:09 +0700

    fixing

--- Swiftui_movie/Views/CustomerProfileTab.swift
+++ Swiftui_movie/Views/CustomerProfileTab.swift
@@ -8,38 +8,85 @@ struct CustomerProfileTab: View {
     @ObservedObject var userSettings = UserSettings()
     var body: some View {
         VStack{
-            Image("aji")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 150, height: 150)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 10)
-            .padding()
-            VStack {
-                
-                HStack {
-                    VStack(alignment: .leading, spacing: 0) {
+            ScrollView{
+                VStack{
+                    Image("aji")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 10)
+                        .padding()
+                    VStack {
                         
-                        Text(userSettings.email)
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
-                        
-                        Text(userSettings.desc)
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-                        
-                        
-                    }.layoutPriority(100)
+                        HStack {
+                            VStack(alignment: .leading, spacing: 0) {
+                                
+                                Text(userSettings.email)
+                                    .font(.title)
+                                    //.fontWeight(.bold)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
+                                
+                                Text(userSettings.desc)
+                                    .font(.headline)
+                                    //.fontWeight(.bold)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+                                
+                                
+                            }.layoutPriority(100)
+                            
+                            Spacer()
+                        }.padding()
+                    }
+                    .cornerRadius(10)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), lineWidth: 1))
+                    .padding()
                     
-                    Spacer()
-                }.padding()
+                    
+                    NavigationLink(destination: HistoryBooking()) {
+                        VStack {
+                            
+                            HStack {
+                                VStack(alignment: .center, spacing: 0) {
+                                    
+                                    Text("History Booking")
+                                        .foregroundColor(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)))
+                                    .frame(maxWidth: .infinity)
+                                    
+                                }.layoutPriority(100)
+                                
+                                Spacer()
+                            }.padding()
+                        }
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), lineWidth: 2))
+                        .padding()
+                    }
+                    
+                    NavigationLink(destination: MedicalQusetioner()) {
+                        VStack {
+                            
+                            HStack {
+                                VStack(alignment: .center, spacing: 0) {
+                                    
+                                    Text("Medical Questioner")
+                                        .foregroundColor(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)))
+                                    .frame(maxWidth: .infinity)
+                                    
+                                }.layoutPriority(100)
+                                
+                                Spacer()
+                            }.padding()
+                        }
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)), lineWidth: 2))
+                        .padding()
+                    }
+                    
+                    
+                }
             }
-            .cornerRadius(10)
-            .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), lineWidth: 1))
-            .padding()
         }     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
             .background(Color(.white))
             .navigationBarTitle("Customer Profile",displayMode: .inline)
