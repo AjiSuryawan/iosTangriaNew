e88ce794c9fe8ea718e6d6e132157a42f6929aa4
Aji Suryawan <kazumovie132@gmail.com>
2020-08-04 23:47:18 +0700

    logout problem

--- Swiftui_movie/Views/CustomerProfileTab.swift
+++ Swiftui_movie/Views/CustomerProfileTab.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 // swiftlint:disable multiple_closures_with_trailing_closure
 struct CustomerProfileTab: View {
-    
+    @State private var isAlert = false
     @ObservedObject var userSettings = UserSettings()
     var body: some View {
         VStack{
@@ -19,7 +19,6 @@ struct CustomerProfileTab: View {
                         .shadow(radius: 10)
                         .padding()
                     VStack {
-                        
                         HStack {
                             VStack(alignment: .leading, spacing: 0) {
                                 
@@ -32,30 +31,21 @@ struct CustomerProfileTab: View {
                                     .font(.headline)
                                     //.fontWeight(.bold)
                                     .foregroundColor(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-                                
-                                
                             }.layoutPriority(100)
-                            
                             Spacer()
                         }.padding()
                     }
                     .cornerRadius(10)
                     .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), lineWidth: 1))
                     .padding()
-                    
-                    
                     NavigationLink(destination: HistoryBooking()) {
                         VStack {
-                            
                             HStack {
                                 VStack(alignment: .center, spacing: 0) {
-                                    
                                     Text("History Booking")
                                         .foregroundColor(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)))
                                     .frame(maxWidth: .infinity)
-                                    
                                 }.layoutPriority(100)
-                                
                                 Spacer()
                             }.padding()
                         }
@@ -84,13 +74,35 @@ struct CustomerProfileTab: View {
                         .padding()
                     }
                     
-                    
+                    Button(action: {
+                        self.isAlert = true
+                        UserDefaults.standard.removeObject(forKey: "username")
+                        UserDefaults.standard.removeObject(forKey: "email")
+                        UserDefaults.standard.removeObject(forKey: "desc")
+                    }) {
+                        LogoutButtonContent()
+                    }
                 }
             }
-        }     .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-            .background(Color(.white))
-            .navigationBarTitle("Customer Profile",displayMode: .inline)
-        
+            if self.isAlert{
+                //SplashScreen()
+            }
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        .background(Color(.white))
+        .navigationBarTitle("Customer Profile",displayMode: .inline)
+    }
+    
+    struct LogoutButtonContent: View {
+        var body: some View {
+            Text("log out")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color.black)
+                .cornerRadius(35.0)
+        }
     }
     
     struct GameListFavTab_Previews: PreviewProvider {
