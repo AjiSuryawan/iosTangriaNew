b3174d160612bb83d221e92950e1ebf9ade3487a
Aji Suryawan <kazumovie132@gmail.com>
2020-09-02 11:21:29 +0700

    cek

--- Swiftui_movie/Views/ListTime.swift
+++ Swiftui_movie/Views/ListTime.swift
@@ -10,6 +10,7 @@ import Foundation
 import SwiftUI
 
 struct ListTime: View {
+    @State private var showDetails = false
     @State private var isAlert = false
     @State private var countryindex = 0
     var countries = ["10.00", "11.00" , "13.00", "14.00", "16.00" , "18.00"];
@@ -19,57 +20,106 @@ struct ListTime: View {
     var rkManager4 = RKManager(calendar: Calendar.current, minimumDate: Date(), maximumDate: Date().addingTimeInterval(60*60*24*365), mode: 0)
     @State var selectedDate = Date()
     var body: some View {
-        ZStack{
-            VStack {
-                Button(action: { self.singleIsPresented.toggle() }) {
-                    Text("Silahkan Pilih Tanggal").foregroundColor(.blue)
+        LoadingView(isShowing: .constant(showDetails)) {
+            ZStack{
+                VStack {
+                    Button(action: { self.singleIsPresented.toggle() }) {
+                        Text("Silahkan Pilih Tanggal").foregroundColor(.blue)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+                    .sheet(isPresented: self.$singleIsPresented, content: {
+                        RKViewController(isPresented: self.$singleIsPresented, rkManager: self.rkManager1)}
+                    )
+                    if self.getTextFromDate(date: self.rkManager1.selectedDate) == "kosongan" {
+                        
+                    }else{
+                        Text(self.getTextFromDate(date: self.rkManager1.selectedDate))
+                            .padding()
+                        //koding ambil tanggal
+                        Text("jam yang anda pilih : "+self.countries[self.countryindex])
+                        Picker (selection: self.$countryindex, label: EmptyView()) {
+                            ForEach(0..<self.countries.count) {
+                                Text(self.countries[$0])
+                                    .tag($0)
+                            }
+                        }.labelsHidden()
+                        Button(action: {
+                            print("Book Now")
+                            self.showDetails = true
+                            //self.isAlert = true
+                            
+                        }) {
+                            Text("Book Now")
+                                
+                                .frame(maxWidth: .infinity)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.green)
+                                .cornerRadius(15.0)
+                        }.disabled(self.countryindex < 0)
+                    }
                 }
                 .padding()
-                .background(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
-                .sheet(isPresented: self.$singleIsPresented, content: {
-                    RKViewController(isPresented: self.$singleIsPresented, rkManager: self.rkManager1)}
-                )
-                if self.getTextFromDate(date: self.rkManager1.selectedDate) == "kosongan" {
-                    
-                }else{
-                    Text(self.getTextFromDate(date: self.rkManager1.selectedDate))
-                        .padding()
-                    //koding ambil tanggal
-                    Text("jam yang anda pilih : "+self.countries[countryindex])
-                    Picker (selection: $countryindex, label: EmptyView()) {
-                        ForEach(0..<countries.count) {
-                            Text(self.countries[$0])
-                                .tag($0)
-                        }
-                    }.labelsHidden()
-                    Button(action: {
-                        print("Book Now")
-                        self.isAlert = true
-                    }) {
-                        Text("Book Now")
-                            
-                            .frame(maxWidth: .infinity)
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.green)
-                            .cornerRadius(15.0)
-                    }.disabled(countryindex < 0)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
+                .background(Color(.white))
+                .navigationBarTitle("Available Time",displayMode: .inline)
+                .onAppear(perform: self.startUp)
+                .navigationViewStyle(StackNavigationViewStyle())
+                
+                if self.isAlert {
+                    MainView()
                 }
+                
             }
-            .padding()
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .top)
-            .background(Color(.white))
-            .navigationBarTitle("Available Time",displayMode: .inline)
-            .onAppear(perform: startUp)
-            .navigationViewStyle(StackNavigationViewStyle())
-            
-            if self.isAlert {
-                MainView()
-            }
-            
+        }
+        
+    }
+    
+    struct ActivityIndicator: UIViewRepresentable {
+        
+        @Binding var isAnimating: Bool
+        let style: UIActivityIndicatorView.Style
+        
+        func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+            return UIActivityIndicatorView(style: style)
+        }
+        
+        func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+            isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
         }
     }
+    struct LoadingView<Content>: View where Content: View {
+        
+        @Binding var isShowing: Bool
+        var content: () -> Content
+        
+        var body: some View {
+            GeometryReader { geometry in
+                ZStack(alignment: .center) {
+                    
+                    self.content()
+                        .disabled(self.isShowing)
+                        .blur(radius: self.isShowing ? 3 : 0)
+                    
+                    VStack {
+                        Text("Loading...")
+                        ActivityIndicator(isAnimating: .constant(true), style: .large)
+                    }
+                    .frame(width: geometry.size.width / 2,
+                           height: geometry.size.height / 5)
+                        .background(Color.secondary.colorInvert())
+                        .foregroundColor(Color.primary)
+                        .cornerRadius(20)
+                        .opacity(self.isShowing ? 1 : 0)
+                    
+                }
+            }
+        }
+        
+    }
+    
     func startUp() {
         
         // example of pre-setting selected dates
